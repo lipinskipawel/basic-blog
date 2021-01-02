54956aafc9ea66202262f7c3b9ea59ecb931cbd4
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-02 22:03:55 +0100

    Add event handlers for buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const handleClick = (event) => {
+        console.log("Hello everyone", event);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello ${name}`);
+        console.log(e.target); // target is the in this case an "html" button
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => {
+                handleClickAgain('mario', e);
+            }}>Click me again</button>
         </div>
     );
 }
