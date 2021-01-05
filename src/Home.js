bf6bed6b76568fa9e86138cfd607aa56b6a13ae6
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-05 19:58:39 +0100

    Create shared BlogList component

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,17 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        { title: "First", body: "Description", author: 'mario', id: 1 },
-        { title: "Second", body: "Some Description", author: 'lugig', id: 2 },
-        { title: "Third", body: "Next Description", author: 'mario', id: 3 }
+        {title: "First", body: "Description", author: 'mario', id: 1},
+        {title: "Second", body: "Some Description", author: 'lugig', id: 2},
+        {title: "Third", body: "Next Description", author: 'mario', id: 3}
     ]);
 
     return (
         <div className="home">
-
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All blogs!" />
         </div>
     );
 }
