c0f1ed4900e72749d22ab493a11c19da3d92d16d
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-11 18:49:52 +0100

    Add loading message while waiting for fetch

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,29 @@
-import {useState, useEffect} from 'react';
-import BlogList from './BlogList';
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    const [blogs, setBlogs] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                console.log(data);
-            })
-    }, []);
-
-    return (
-        <div className="home">
-            { blogs && <BlogList blogs={blogs} title="All blogs!" />}
-        </div>
-    );
-}
+  return (
+    <div className="home">
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+    </div>
+  );
+};
 
 export default Home;
