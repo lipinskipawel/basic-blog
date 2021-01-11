8b816c262eff373a807abed6ea6b5020de61f2e8
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-11 19:07:34 +0100

    Check for any error from the API and update UI

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,25 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
           setIsPending(false);
         });
     }, 1000);
@@ -20,6 +30,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
