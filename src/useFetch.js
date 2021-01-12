4016a64f38f7c4c12e4487bec697ce68b81b2ae8
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-12 18:47:54 +0100

    Create custom react hook useFetch for API call

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+// custom react hook must start with the 'use' word
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, [url]); // url means that whenever the 'url' changes it's going to rerun this function 'useEffect'
+
+  return { data, isPending, error }; // we could also return the array. Passing the object means that
+  // the order of properties doesn't matter in case of destructure them later
+};
+
+export default useFetch;
