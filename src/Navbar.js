946f924cabf0da8eadf2fed77ed54443d13563e5
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-15 18:26:16 +0100

    Use Link component to intercept https request

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,25 @@
+import { Link } from "react-router-dom";
+
 // stateless functional component
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <h1>Basic blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px'
-                }}>New Blog</a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="navbar">
+      <h1>Basic blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
