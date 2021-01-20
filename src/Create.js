2e52f3efed620a95a53cb7b19be14f4a773547fc
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-20 18:51:29 +0100

    Disabled the button when user submit the blog

--- src/Create.js
+++ src/Create.js
@@ -4,19 +4,24 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     // preventing page being refreshed
     e.preventDefault();
     const blog = { title, body, author };
+    setIsPending(true);
 
-    fetch("http://localhost:8000/blogs", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(blog),
-    }).then(() => {
-      console.log("new blog added");
-    });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blog),
+      }).then(() => {
+        setIsPending(false);
+        console.log("new blog added");
+      });
+    }, 1000);
   };
 
   return (
@@ -41,7 +46,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="luigi">luigi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
