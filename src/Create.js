aebeb922d745249608a39bfd7f72498549208906
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-20 18:37:58 +0100

    Allows to create new blogs

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,24 @@ const Create = () => {
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
 
+  const handleSubmit = (e) => {
+    // preventing page being refreshed
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+    });
+  };
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -28,9 +42,6 @@ const Create = () => {
           <option value="luigi">luigi</option>
         </select>
         <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
       </form>
     </div>
   );
