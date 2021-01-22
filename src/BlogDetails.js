7b79e75f134b4454eb58c0a26f6f27f164db4238
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-22 18:07:06 +0100

    Allow to delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -7,6 +7,16 @@ const BlogDetails = () => {
   const { data: blog, error, isPending } = useFetch(
     `http://localhost:8000/blogs/${id}`
   );
+  const history = useHistory();
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -16,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={() => handleDelete(blog.id)}>Delete</button>
         </article>
       )}
     </div>
